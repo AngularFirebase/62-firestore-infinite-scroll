4eddf5e2048031d91d39edbfc7c98c3cbf3f6fe2
codediodeio <hello@codediode.io>
2017-10-29 06:44:22 -0700

    tested working demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,17 @@ export class AppComponent implements OnInit {
   constructor(public page: PaginationService) {}
 
   ngOnInit() {
-    this.page.init('boats', 'year', { reverse: true, prepend: false })
+    this.page.init('boats', 'year', { reverse: false, prepend: false })
   }
 
   scrollHandler(e) {
     if (e === 'bottom') {
       this.page.more()
     }
+
+    // if (e === 'top') {
+    //   this.page.more()
+    // }
   }
 
 
